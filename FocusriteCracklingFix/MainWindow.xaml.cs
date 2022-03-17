3a938860b4ef6e79417a06dd4e8ac70e8764aeeb
Ingo Wolfgang <ingo.wolfgang@t-online.de>
2022-03-17 17:14:47 +0100

    Initial Commit

---
+++ FocusriteCracklingFix/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FocusriteCracklingFix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Hide();
+
+
+            bool SkipTask = false;
+            bool RetryFix = true;
+
+            string[] args = Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                if (arg.ToLower().Contains("skiptask"))
+                {
+                    SkipTask = true;
+                }
+                if (arg.ToLower().Contains("dontretryfix"))
+                {
+                    RetryFix = false;
+                }
+            }
+
+            if (!SkipTask)
+            {
+                SetUpTask();
+            }
+
+            FixAudio(RetryFix);
+
+            Environment.Exit(0);
+        }
+
+        public static void SetUpTask()
+        {
+            try
+            {
+                TaskService tService = new TaskService();
+                TaskDefinition tDefinition = tService.NewTask();
+
+                string TaskName = "FocusriteCracklingFix";
+                string TaskDescription = "Sets process priority to high and process affinity to a single core (audiodg.exe).";
+                string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                string exeLocation = Process.GetCurrentProcess().MainModule.FileName;
+
+                tDefinition.Principal.Id = userName;
+                tDefinition.Principal.DisplayName = userName;
+                tDefinition.Principal.LogonType = TaskLogonType.InteractiveToken;
+                tDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+                tDefinition.RegistrationInfo.Description = TaskDescription;
+
+                tDefinition.Triggers.Add(new LogonTrigger());
+                tDefinition.Actions.Add(new ExecAction(exeLocation));
+
+                tDefinition.Settings.MultipleInstances = TaskInstancesPolicy.IgnoreNew;
+                tDefinition.Settings.DisallowStartIfOnBatteries = false;
+                tDefinition.Settings.StopIfGoingOnBatteries = false;
+                tDefinition.Settings.StartWhenAvailable = true;
+                tDefinition.Settings.RunOnlyIfNetworkAvailable = false;
+                tDefinition.Settings.IdleSettings.StopOnIdleEnd = false;
+                tDefinition.Settings.IdleSettings.RestartOnIdle = false;
+
+                tDefinition.Settings.AllowDemandStart = true;
+                tDefinition.Settings.Enabled = true;
+                tDefinition.Settings.Hidden = false;
+                tDefinition.Settings.RunOnlyIfIdle = false;
+                tDefinition.Settings.WakeToRun = false;
+                tDefinition.Settings.Priority = ProcessPriorityClass.High;
+
+
+                tService.RootFolder.RegisterTaskDefinition(TaskName, tDefinition, TaskCreation.CreateOrUpdate, userName);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Failed to set up Task.\n\n" + e.ToString(), "FocusriteCracklingFix");
+            }
+        }
+
+        public static void FixAudio(bool RunInLoopIfNoProcessesFound)
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("audiodg");
+                if (processes.Length == 0)
+                {
+                    System.Threading.Tasks.Task.Run(() => System.Threading.Tasks.Task.Delay(15 * 1000)).Wait();
+                    if (RunInLoopIfNoProcessesFound)
+                    {
+                        FixAudio(RunInLoopIfNoProcessesFound);
+                    }
+                }
+                foreach (Process p in processes)
+                {
+                    if (p.PriorityClass != ProcessPriorityClass.High)
+                    {
+                        p.PriorityClass = ProcessPriorityClass.High;
+                    }
+
+                    p.ProcessorAffinity = (IntPtr)4; // works lmao. Uses just the second (third) Core / Thread
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Failed to change audiodg.exe.\n\n" + e.ToString(), "FocusriteCracklingFix");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+﻿### About The Project
+
+Hi! I've run into the issue where your mic makes a "crackling" sound in most VoIP Applications (TeamSpeak, Discord, Zoom etc.) when using a Focusrite Scarlett Interface.
+
+There are a lot of reports of this bug with various fixes online, however most of them didn't work for me and, according to the comments, didnt work for a majority of the userbase.
+
+The only reliable fix I found was setting the process priority of "audiodg.exe" to "High" and setting its core affinity so that only one core is used.
+
+I assume the issue appears because the interface drivers run into issues when syncing their clock / refreshrate? Just a guess.
+
+As always, fuck focusrite and remember to not buy from Thomann.
+
+Have a nice day.
+
+### What this does
+
+* Adds / Edits a scheduled Task, which just runs itself on every user logon with highest available privileges. 
+* Loops through all processes looking for processes called "audiodg.exe"
+* If it doesnt find any, it will wait 15 seconds and try again. This loops infinitively.
+* Sets the process priority to HIGH and its core affinity to use only the CPU 2 (third CPU core / thread)
+
+* This probably only works on 64 Bit Windows 10 and higher.
+
+### How to run
+
+* Download the latest .exe [here](https://google.com)
+* Put it into a folder (a permanent place, not just your Download Path)
+* Run the .exe once.
+* Done. It will automatically start itself on every reboot. So dont move the .exe.
+* If you do end up having to move your .exe, just run manually once.
+
+### License
+
+Licensed under MIT, do whatever you want with this.
